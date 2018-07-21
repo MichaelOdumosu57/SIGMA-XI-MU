030e8143b1b25384bc95c06b361676ccf7fd9845
Michael Odumosu <michaelodumosu57@gmail.com>
2018-07-21 15:09:57 -0400

    minor change

--- API/format.js
+++ API/format.js
@@ -1,4 +1,4 @@
-// left off with manipulating adv clone to understandin how to dynamic change the needed dives
+// left off with manipulating adv clone to understanding how to dynamic change the needed dives
 
 
 
@@ -7,10 +7,10 @@
 //  standard format for how all your website should be bulit, should be a certain format, but made like this for best visual appearance and editing purposes. this is not for web objects but for dynamic concepts needed to achieve the purpose
 
                 // helper function
-                    
+
                     function listening_zoom(){
                         var know_change = (browser_window.visualViewport.width/2) - (browser_window.outerWidth/2)
-                        
+
                         if(know_change != $(".html_page").offset()["left"]){
                             $(".html_page").offset({
                                 left:  know_change
@@ -18,12 +18,12 @@
                         }
 
                     }
-                    
+
                 ////////////////////////////////////////////////////////////////////
                 // this function helps the browser know how to resize the html window
                 // know_change is the flag that lets the function know that the zoom has changed
                 ////////////////////////////////////////////////////////////////////
- 
+
  $(document).ready(function(){
             // position fix
             $("body > *:not('.debug')").css({
@@ -35,17 +35,17 @@
             ////////////////////////////////////////////////////////////////////
             // global fix for purposeful programming
             ////////////////////////////////////////////////////////////////////
-     
+
      //the html container
      {
-        
+
     $(".html_page").length > 0 ?  true : $("body").append("<div class = 'html_page'></div>")
     $(".html_page").css({
         "width":  browser_window.outerWidth,
         "height":  browser_window.outerHeight,
         "border": "3px solid rgb(227,227,227)"
     })
-    
+
     setInterval(listening_zoom,.00001)
         // the debugger object
             $(".html_page").after("<div class ='debug'></div>")
@@ -73,11 +73,11 @@
                 "left":numberParse($(".html_page").css("width"))
                 }
             ]
-            
+
         {
             // global helper function
                 function changing_math (counter,iterate_object,direction){
-                    
+
                     if (direction == "+"){
                         if(counter == iterate_object.length - 1){
                             counter = 0
@@ -94,7 +94,7 @@
                         else{
                             counter -= 1
                         }
-                        
+
                     }
                     else{
                         throw new error("signs man")
@@ -107,7 +107,7 @@
             // this function is made becuase it is timely to put conditionals everytime you need to iterate through
             // an array based on events, once the counter gets to the length of an array or that type, it will start at 0 again and vice versa
             ///////////////////////////////////////////////////////////////
-            
+
             // helper function
                 function debug_reset(){
                     debug_executed = false
@@ -117,9 +117,9 @@
             // this function helps the program properly reset the debugger when it needs to be put away
             //  or I could make it draggable
             ///////////////////////////////////////////////////////////////
-            
+
             //helper function
-            
+
                 function event_linker(){
                 debug["debug object.self"][1] ? console.log("looking to clear") : ""
                 // $(".debug_button").length != 0 ? clearInterval(object_looker) : ""
@@ -137,13 +137,13 @@
             // this function links the event handler to the uncreated div
             //  for some reason you just have to keep calling this to make sure it still attached to the button
             ///////////////////////////////////////////////////////////////
-            
+
             // test object
             $(".debug").css(debug_properties[0]).after("<h1 style = 'font-size:5em; color:blue'> Testing text</h1>")
             ///////////////////////////////////////////////////////////////
             // an idea for the test object it should be mainly reloading the page
             ///////////////////////////////////////////////////////////////
-            
+
             $(".debug").click(function(event){
                 if(debug_executed == true ){
                     return
@@ -168,7 +168,7 @@
                 "<div class ='well booleans'></div>" +
                 '<a class="  carousel-control" role="button" data-slide="next" style="right: 0; left: auto;" ><span class="glyphicon glyphicon-chevron-right" aria-hidden="true" ></span><span class="sr-only" ></span></a>'+
                 '<a class="  carousel-control" role="button" data-slide="prev" ><span class="glyphicon glyphicon-chevron-left" aria-hidden="true" ></span><span class="sr-only" ></span></a>')
-            
+
                 $(".topic").css({
                     "width":"20em",
                     "height":"5em",
@@ -203,38 +203,38 @@
                     console.log(event,debug)
                 })
                 $(".glyphicon-chevron-left").click(function(){
-                    
+
                     $(".topic h2").text(topic[changing_math(debug_counter,topic,"-")])
                     $(".booleans h2").text(booleans[debug_counter])
-                    
+
                 })
                 $(".glyphicon-chevron-right").click(function(){
-                    
+
                     $(".topic h2").text(topic[changing_math(debug_counter,topic,"+")])
                     $(".booleans h2").text(booleans[debug_counter])
-                    
+
                 })
                 let debug_height_check = numberParse($(".debug").css("height"))
                 $(".glyphicon").click(function(){
                     debug["debug object.self"][0] ? console.log(booleans[debug_counter]) : ""
                         debug["debug object.self"][1] ? console.log("hit",booleans[debug_counter].length,$(".booleans").length)  : ""
-                    
-                         
+
+
                         $(".booleans:not(:first)").remove()
                         $(".booleans").adv_clone({
                             items:booleans[debug_counter].length
                         })
-                        
-                        
+
+
                         var debug_height_check_increase= 2.4 * numberParse($(".booleans").css("height")) * $(".booleans").length
-                            
+
                         $(".debug").css({
                             "height": (debug_height_check > debug_height_check_increase ? debug_height_check : debug_height_check_increase),
                             "border-radius":1.7* numberParse($(".booleans").css("height")) * $(".booleans").length
                         })
-                    
-                    
-                    
+
+
+
                 })
                 $(".debug").append("<div class = 'debug_button'><h2>Return</h2></div>")
                 $(".debug_button > h2").css({
@@ -249,12 +249,12 @@
                     "margin-left":"10em"
                 })
             })
-            
+
             var object_looker = setInterval(event_linker,1100)
-            
-                
-            
-            
+
+
+
+
         }
         //////////////////////////////////////////////////////////////////////////////////////////
         // I am making a debug object to better debug any problems along the way in making this API
@@ -265,8 +265,8 @@
         // var debug_counter helpes the object know when to move to the next topics in debug
         //  var object_looker helps clear the interval used for the browser to listen for the missing debug button
         //////////////////////////////////////////////////////////////////////////////////////////
-    
-    
+
+
         // external objects
         {
             var topspace = 0;
@@ -276,19 +276,19 @@
                     $(outside).css({
                         "position":"relative",
                         "top":topspace += numberParse($("body > *").eq(no -1).css("height"))
-                        
+
                     })
                     debug["external objects"][1] ? console.log("move it out",$(outside).css("position")): ""
                 }
             })
-                
+
         }
         ////////////////////////////////////////////////////////////////////
         // this section focus on ensure that anything outside html container tag stays outside in the appropriate fashion
         // var topspace ensures that all outside contents are placed in an orderly fashion
         ////////////////////////////////////////////////////////////////////
-        
-        
+
+
     }
     ////////////////////////////////////////////////////////////////////
     // the html container was designed for the problem presented by zooming functionality of different websites and the media device dimensions presented
@@ -298,19 +298,19 @@
         // mobile responiveness
         // item placement suggestion
         // zoom size
-        
+
     // browser_window.visualViewport.width is responsible for the width ratio change when the page zoom is changed
     //  leading_navigation will be nacigation control, if it refuses to stay in the html container something must be done
     ////////////////////////////////////////////////////////////////////
-    
+
      /*FIX ME */
     // in firefox window the outerwidth is not the same find a way around this
     ////////////////////////////////////////////////////////////////////
-    
+
     //test object
         // $(".debug").trigger("click")
     ////////////////////////////////////////////////////////////
     // running checks after the API has been built
     ////////////////////////////////////////////////////////////
-    
+
 })
